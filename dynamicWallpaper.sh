4af7773d58c452a88ab1f8536ec44bc7ecf96002
Kia Hamedi <kia.arta9793@gmail.com>
2018-10-24 23:50:59 +0330

    add script

---
+++ dynamicWallpaper.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+while true
+do
+  hour=`date +"%H"`
+  if [ $hour == 6 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_1.jpeg
+  fi
+  if [ $hour == 7 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_2.jpeg
+  fi
+  if [ $hour == 8 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_3.jpeg
+  fi
+  if [ $hour == 9 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_4.jpeg
+  fi
+  if [ $hour == 10 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_5.jpeg
+  fi
+  if [ $hour == 11 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_6.jpeg
+  fi
+  if [ $hour == 12 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_7.jpeg
+  fi
+  if [ $hour == 13 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_8.jpeg
+  fi
+  if [ $hour == 14 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_9.jpeg
+  fi
+  if [ $hour == 15 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_10.jpeg
+  fi
+  if [ $hour == 16 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_11.jpeg
+  fi
+  if [ $hour == 17 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_12.jpeg
+  fi
+  if [ $hour == 18 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_13.jpeg
+  fi
+  if [ $hour == 19 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_14.jpeg
+  fi
+  if [ $hour == 20 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_15.jpeg
+  fi
+  if [ $hour == 21 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_16.jpeg
+  fi
+  if [ $hour == 22 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_16.jpeg
+  fi
+  if [ $hour == 23 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_16.jpeg
+  fi
+  if [ $hour == 00 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_16.jpeg
+  fi
+  if [ $hour == 1 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_16.jpeg
+  fi
+  if [ $hour == 2 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_16.jpeg
+  fi
+  if [ $hour == 3 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_16.jpeg
+  fi
+  if [ $hour == 4 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_16.jpeg
+  fi
+  if [ $hour == 5 ]
+  then
+      gsettings set org.mate.background picture-filename /home/kia/Desktop/photos/mojave_dynamic_16.jpeg
+  fi
+
+  sleep 60
+done
